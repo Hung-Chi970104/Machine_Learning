187baf56d3cbf4f029d6d72325302708e20b47be
Hung-Chi970104 <hungchi970104@gmail.com>
2025-05-31 10:39:37 -0400

    PYQT setup

--- PYQT/1_Setup_Basics.py
+++ PYQT/1_Setup_Basics.py
@@ -4,4 +4,15 @@ import sys
 
 def window():
     app = QApplication(sys.argv)
-    
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("Hung-Chi First Setup")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("my first label")
+    label.move(50, 50)
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
